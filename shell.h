c4a423ee946801746ab14ec1ac365fd29bf20bc4
Chymezy <chymezy332@gmail.com>
2023-11-05 02:17:01 -0800

    adopting one single main function

--- shell.h
+++ shell.h
@@ -12,7 +12,7 @@ void _execute(char *command, char **argv, char *paths);
 char *getpath(char *command, char *paths);
 char *_strcat(char *string, char *add);
 int _strcmp(char *first, char *second);
-char *_strdup(char *des, char *orig);
+char *_strdup(const char *);
 char *_strcpy(char *des, char *orig);
 int _strlen(char *string);
 
